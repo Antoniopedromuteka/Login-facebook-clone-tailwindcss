2523757babe6ddcdd737fef3a48520e36890e711
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-09-12 00:04:39 +0100

    finalizando as customizacoes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,25 @@ module.exports = {
   theme: {
     extend: {
       spacing:{
-         
+         "30": "7.5rem",
+         "290px": "290px",
+         "396px": "396px",
+         "500px": "500px",
+         "980px": "980px"
+      },
+
+      fontSize:{
+        "subtitle": "1.7rem"
       },
       colors: {
-        
+        "main-gray": "#f0f2f5",
+        "main-blue": "#1877f2",
+        "main-blue-hover": "#166fe5",
+        "main-green": "#42b72a",
+        "main-green-hover": "#36a420"
       },
-      backgroundImage: theme =>({
-     
-      }),
+
+       
       backgroundSize:{
         
       },
