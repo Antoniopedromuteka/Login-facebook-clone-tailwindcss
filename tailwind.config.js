c241a6d2abbb438bfbfbbe3f6da48bb054e666e3
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-09-11 23:52:29 +0100

    criando configuracao de pastas

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing:{
+         
+      },
+      colors: {
+        
+      },
+      backgroundImage: theme =>({
+     
+      }),
+      backgroundSize:{
+        
+      },
+      backgroundPosition:{
+   
+      },
+      fontSize:{
+   
+      }
+    },
+  },
+  plugins: [],
+}
